1e38d522d5b75beeb57f6852d50f77f28c438ac8
Ald28 <aldopichihua123@gmail.com>
2025-06-20 09:05:47 -0500

    diseño

--- src/api/api_admin.js
+++ src/api/api_admin.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/auth/admin";
-const API_PSICO = "http://localhost:8080/auth/psicologo";
+const API = "https://tesis-2025.onrender.com/auth/admin";
+const API_PSICO = "https://tesis-2025.onrender.com/auth/psicologo";
 //const API_PSICO = "http://localhost:8080/auth/psicologo";
 //const API_PSICO = "https://tesis-2025.onrender.com/auth/psicologo";
 
