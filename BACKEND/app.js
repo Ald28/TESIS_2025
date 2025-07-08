9d3677b32e0a78ced19c9abbca8d44de0f22ce91
Ald28 <aldopichihua123@gmail.com>
2025-07-08 00:18:13 -0500

    espacio

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -15,8 +15,11 @@ const notificacionRoutes = require('./src/routes/notificacion.routes');
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cors({
-  origin: '*',
+  origin: 'https://app.calmatec.es',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
--- BACKEND/src/middlewares/multer.js
+++ BACKEND/src/middlewares/multer.js
@@ -1,6 +1,11 @@
 const multer = require('multer');
 
 const storage = multer.memoryStorage(); // almacena el archivo en memoria para subirlo a Cloudinary
-const upload = multer({ storage });
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 50 * 1024 * 1024
+  }
+});
 
 module.exports = upload;
