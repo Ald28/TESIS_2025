402e83cb7b7beca02e117d55b75745bd4142f1ce
Ald28 <aldopichihua123@gmail.com>
2025-07-08 01:08:32 -0500

    middleware

--- BACKEND/src/routes/metodo_relajacion.routes.js
+++ BACKEND/src/routes/metodo_relajacion.routes.js
@@ -17,7 +17,7 @@ router.get('/recomendados', metodo.listarMetodosRecomendados);
 router.get('/privados-todos', metodo.listarTodosMetodosPrivados);
 
 // Editar actividades recomendadas
-router.put('/actividades/editar/:id', upload.single('archivo'), metodo.editarMetodoRelajacion);
+router.put('/actividades/editar/:id', authPsicologo,upload.single('archivo'), metodo.editarMetodoRelajacion);
 
 // Eliminar método
 router.delete('/actividades/eliminar/:id', metodo.eliminarMetodoRelajacion);
