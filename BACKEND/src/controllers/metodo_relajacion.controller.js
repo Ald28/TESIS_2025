7b3c41dbf28bcf41d07ef544b52b5a2ea717e18f
Ald28 <aldopichihua123@gmail.com>
2025-07-08 00:54:55 -0500

    console

--- BACKEND/src/controllers/metodo_relajacion.controller.js
+++ BACKEND/src/controllers/metodo_relajacion.controller.js
@@ -165,7 +165,7 @@ const editarMetodoRelajacion = async (req, res) => {
         res.status(200).json({ message: 'Método actualizado correctamente.' });
 
     } catch (error) {
-        console.error('Error al editar método de relajación:', error);
+        console.error('Error al editar método de relajación:', error.message || error);
         res.status(500).json({ message: 'Error interno del servidor.' });
     }
 };
