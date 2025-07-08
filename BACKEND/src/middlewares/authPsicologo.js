eba989bae56e10152d8630e22fca471dae4b7d9b
Ald28 <aldopichihua123@gmail.com>
2025-07-08 02:47:15 -0500

    nuevo8

--- BACKEND/src/middlewares/authPsicologo.js
+++ BACKEND/src/middlewares/authPsicologo.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authPsicologo = (req, res, next) => {
   const authHeader = req.headers['authorization'];
+  console.log('🔐 authHeader:', authHeader);
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Token requerido o malformado' });
   }
@@ -10,12 +11,14 @@ const authPsicologo = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('✅ Token decodificado:', decoded);
     if (decoded.rol !== 1) {
       return res.status(403).json({ message: 'Acceso denegado para rol diferente a psicólogo' });
     }
     req.usuario = decoded;
     next();
   } catch (error) {
+    console.error('❌ Error al verificar token:', error);
     return res.status(401).json({ message: 'Token inválido' });
   }
 };
